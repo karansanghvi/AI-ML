fd1cf4192e318f196b22bf8c361aa191a0f16eb3
Karan Sanghvi <111078325+karansanghvi@users.noreply.github.com>
2025-10-14 14:36:47 -0700

    done with EDA and model TRAINING in project

--- Machine Learning Project/src/exception.py
+++ Machine Learning Project/src/exception.py
@@ -1,13 +1,13 @@
 import sys
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc=tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_codee.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name, exc_tb.tb.lineno,str(error)
-        
-        return error_message
-    )
+        file_name, exc_tb.tb.lineno,str(error))
+    
+    return error_message
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
